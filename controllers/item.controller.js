26171d4ab031ae4661579c4cd4f134d8b6483f93
Leo Lee <tl.leolee@gmail.com>
2020-09-10 12:19:16 -0700

    add route and add controller placeholder

---
+++ controllers/item.controller.js
@@ -0,0 +1,27 @@
+exports.create_get = function create_get(req, res) {
+  res.status(500).send('TO BE IMPLEMENTED');
+}
+exports.create_post = function create_post(req, res) {
+  res.status(500).send('TO BE IMPLEMENTED');
+}
+
+exports.update_get = function update_get(req, res) {
+  res.status(500).send('TO BE IMPLEMENTED');
+}
+exports.update_post = function update_post(req, res) {
+  res.status(500).send('TO BE IMPLEMENTED');
+}
+
+exports.delete_get = function delete_get(req, res) {
+  res.status(500).send('TO BE IMPLEMENTED');
+}
+exports.delete_post = function delete_post(req, res) {
+  res.status(500).send('TO BE IMPLEMENTED');
+}
+
+exports.detail = function detail(req, res) {
+  res.status(500).send('TO BE IMPLEMENTED');
+}
+exports.list = function list(req, res) {
+  res.status(500).send('TO BE IMPLEMENTED');
+}
--- routes/catalog.js
+++ routes/catalog.js
@@ -1,6 +1,42 @@
 const express = require('express');
-const router = express.Router();
+const router  = express.Router();
 
+// Require controller modules.
+var category_controller = require('../controllers/category.controller');
+var item_controller     = require('../controllers/item.controller');
 
+/* Index Routes */
+
+router.get('/', category_controller.index);
+
+/* Category Routes */
+
+// create
+router.get ('/category/create', category_controller.create_get);
+router.post('/category/create', category_controller.create_post);
+// update
+router.get ('/category/update', category_controller.update_get);
+router.post('/category/update', category_controller.update_post);
+// delete
+router.get ('/category/delete', category_controller.delete_get);
+router.post('/category/delete', category_controller.delete_post);
+// read
+router.get ('/category/:id'   , category_controller.detail);
+router.get ('/categories'     , category_controller.list);
+
+/* Item Routes */
+
+// create
+router.get ('/item/create'    , item_controller.create_get);
+router.post('/item/create'    , item_controller.create_get);
+// update
+router.get ('/item/update'    , item_controller.update_get);
+router.post('/item/update'    , item_controller.update_get);
+// delete
+router.get ('/item/delete'    , item_controller.delete_get);
+router.post('/item/delete'    , item_controller.delete_get);
+// read
+router.get ('/item/:id'       , item_controller.detail);
+router.get ('/items'          , item_controller.list);
 
 modules.exports = router;
