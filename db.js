fbd6701e3811de99180e0f5bfb8a89d0885ce1ff
Leo Lee <tl.leolee@gmail.com>
2020-09-10 10:54:00 -0700

    setup mongo and catalog route

---
+++ db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connParams = process.env.MONGO_URI;
+mongoose.connect(connParams, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+module.exports = db;
